df5f925c9c144b3fbfad2349e4bf34c826b0ae44
MarkBjj <markmullendev@gmail.com>
2026-07-20 15:11:09 -0300

    dynamic styles

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -19,28 +19,25 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "rgb(2, 13, 30)")};
 `;
 
 const Input = styled.input`
   display: block;
   width: 100%;
   padding: 0.5rem;
-  border: 1px solid #d1d5db;
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#b91c1c" : "#d1d5db")};
   border-radius: 0.25rem;
   font-size: 1rem;
   line-height: 1.5rem;
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "black")};
+  background-color: ${({ $invalid }) =>
+    $invalid ? "rgb(101, 58, 58)" : "white"};
 
   &:focus {
     outline: none;
     border-color: #2563eb;
     box-shadow: 0 0 0 2px rgba(37, 99, 235, 0.5);
-  }
-
-  &.invalid {
-    border-color: #f87171;
-    background-color: #fef2f2;
-    color: #b91c1c;
   }
 `;
 export default function AuthInputs() {
@@ -67,25 +64,23 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <Label className={` ${emailNotValid ? "invalid" : undefined}`}>
-            Email
-          </Label>
+          {/* to style in component, add prop className to Label and Input, then
+          use conditional rendering to add a className when invalid */}
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
+            $invalid={emailNotValid}
             //style={{ backgroundColor: emailNotValid ? "red" : "white" }}
-            className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label className={` ${passwordNotValid ? "invalid" : undefined}`}>
-            Password
-          </Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
+            $invalid={passwordNotValid}
             //style={{ backgroundColor: passwordNotValid ? "red" : "white" }}
             // undefined (not '' or false) so React omits the className attribute entirely when valid
-            className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
