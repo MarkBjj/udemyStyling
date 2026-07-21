8462cbb71eb8d22f3d5ffea57b6eca2781d282bc
MarkBjj <markmullendev@gmail.com>
2026-07-21 19:18:30 -0300

    CSS changes and understanding rem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex flex-col items-center mt-4 mb-4 p-16">
       <img
         src={logo}
-        className="object-contain mb-8 width-44 height-28"
+        className="object-contain mb-8 w-44 h-44"
         alt="A canvas"
       />
       <h1 className="text-3xl font-bold tracking-widest letter-spacing-0.4em text-center uppercase text-amber-950 font-family-Helvetica margin-0">
